360f1316a623fdeae29df6ee2d529e11c569b055
sgraziola <simo.fdl@gmail.com>
2022-10-18 15:17:08 +0200

    Campominato: trying to select all cells with bombs

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,8 +23,8 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 
 //L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 //eventlistener sul button
-    //ciclo for per generare griglia
-    //ad ogni indice assegno un num progressivo da 1 a 10
+//ciclo for per generare griglia
+//ad ogni indice assegno un num progressivo da 1 a 10
 
 
 //seleziono l'elemento della DOM per eventListener
@@ -35,48 +35,58 @@ const levelEl = document.getElementById("levels");
 
 
 //applico event listener al click sul button
-generateElement.addEventListener("click", function (){
-    containerElement.innerHTML= "";
+generateElement.addEventListener("click", function () {
+    containerElement.innerHTML = "";
     const level = levelEl.value;
     let maxCellsNumb;
-    if(level == "1"){
+    if (level == "1") {
         maxCellsNumb = 100;
-    } else if(level == "2"){
+    } else if (level == "2") {
         maxCellsNumb = 81;
     } else {
         maxCellsNumb = 49;
     }
-    let rowLenght= Math.sqrt(maxCellsNumb);
+    let rowLenght = Math.sqrt(maxCellsNumb);
+    //Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
     const bombs = [];
     while (bombs.length !== 16) {
         const bomb = randomNumb(1, maxCellsNumb);
         //console.log(bomb);
-        if(!bombs.includes(bomb)){
+        if (!bombs.includes(bomb)) {
             bombs.push(bomb);
-        } 
+        }
     }
     console.log(bombs);
-    for (let i = 0; i< maxCellsNumb; i++){
+
+    
+    for (let i = 0; i < maxCellsNumb; i++) {
         const cell = divGenerator(i + 1);
         cell.style.setProperty("width", `calc(100% / ${rowLenght})`);
         containerElement.insertAdjacentElement("beforeend", cell);
-        //Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
         
-        /* In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle. */
-        cell.addEventListener("click", function findBombs(){
+        const cellBombs = [];
+        for (let j = 0; j < bombs.length; j++) {
             if (bombs.includes(Number(cell.innerHTML))) {
-                cell.classList.add("red");
+                cellBombs.push(cell);
+                console.log(cellBombs);
+            }
+        }
+        console.log(cellBombs);
+        /* In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle. */
+        cell.addEventListener("click", function () {
+            if (!bombs.includes(Number(cell.innerHTML))) {
+                cell.classList.add("light_blue");
                 console.log(cell.innerText);
             } else {
-            cell.classList.add("light_blue");      
-            console.log(cell.innerText);
+                cell.classList.add("red");
+                console.log(cell.innerText);
             }
         })
     }
 }
 );
 
-
+// se la partita termina allora tutte le celle che contengono bomb si colorano di rosso
 
 
 /* 
@@ -90,7 +100,7 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
  * @param {number} num un numero intero
  * @returns {Element} dom element
  */
-function divGenerator (num){
+function divGenerator(num) {
     const cellElement = document.createElement("div");
     cellElement.classList.add("cell");
     cellElement.insertAdjacentText("beforeend", num);
@@ -98,6 +108,6 @@ function divGenerator (num){
 }
 
 function randomNumb(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
